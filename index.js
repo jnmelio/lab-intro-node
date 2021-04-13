8dd7238f4a532958c68f80f806c0cb91f7c108a2
Julie <julienonnenmacher@hotmail.fr>
2021-04-13 17:47:00 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [],
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort((element1, element2) => {
+      if(element1 > element2) {
+        return 1
+      } else {
+        return -1
+      }
+    })
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.items.length) {
+      return this.items.indexOf(pos)
+    } else {
+      throw new Error('OutOfBounds')
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length > 0 ) {
+      return Math.max(...this.items)
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length > 0 ) {
+      return Math.min(...this.items)
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0
+    for (let i = 0; i <= this.items.length; i++) {
+      sum += i
+    }
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length > 0) {
+      let avg = 0
+      for (let i = 0; i <= this.items.length; i++) {
+        avg += i
+      }
+      return avg / this.items.length
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 }
 
 module.exports = SortedList;
